c85d5a55721d6c8b1500417ba11858ab75450664
“ankit-HM-JC” <ankit.singh@jumpcloud.com>
2022-06-06 07:08:35 +0530

    added more tools

---
+++ src/components/MainNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="py-8 border-b-2">
+    <a class="mx-12 text-3xl">Logo</a>
+    <a class="mx-6 text-lg font-light">Team</a>
+    <a class="mx-6 text-lg">Location</a>
+    <a class="mx-6 text-lg">Jobs</a>
+    <a class="mx-6 text-lg">Students</a>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
