8f6c1cbffe914ee5caf0bb545d072d0f79a2cf77
“ankit-HM-JC” <ankit.singh@jumpcloud.com>
2022-06-08 08:04:14 +0530

    added nav-items and tests

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,9 +4,26 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a data-testid="logo" class="flex items-center h-full text-xl"
-          >G Careers</a
+        <a
+          :href="url"
+          data-testid="logo"
+          class="flex items-center h-full text-xl"
+          >{{ company }}</a
         >
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li
+              v-for="(menu, index) in menuItems"
+              :key="index"
+              class="h-full ml-9"
+              data-testid="nav-menu-items-list"
+            >
+              <a href="" class="flex items-center h-full py-2.5">
+                {{ menu }}
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -15,5 +32,18 @@
 <script>
 export default {
   name: "MainNav",
+  data() {
+    return {
+      company: "G Careers",
+      url: "https://google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at G Careers",
+        "Students",
+        "Jobs",
+      ],
+    };
+  },
 };
 </script>
--- tests/unit/components/MainNav.spec.js
+++ tests/unit/components/MainNav.spec.js
@@ -22,4 +22,26 @@ describe("MainNav", () => {
     // 2 approach to test text logo
     expect(wrapper.find('[data-testid="logo"]').text()).toMatch("G Careers");
   });
+
+  it("Should render list items of links inside navigation", () => {
+    const wrapper = shallowMount(MainNav);
+    const navLinkItems = wrapper.findAll("li");
+    const navLinkItemsWithTest = wrapper.findAll(
+      '[data-testid="nav-menu-items-list"]'
+    );
+    const navTextss = navLinkItemsWithTest.map((navLinkItem) =>
+      navLinkItem.text()
+    );
+    console.log("navTexts", navTextss);
+    const navTexts = navLinkItems.map((navLinkItem) => navLinkItem.text());
+    // toEqual is uded to compare arrays and objects
+    // toBe is used to compare premitives data types
+    expect(navTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at G Careers",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
